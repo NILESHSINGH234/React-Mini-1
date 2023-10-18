311b55f51d0104be82cada7b197cc4d65fe88ffb
NILESHSINGH234 <nilesh01june@gmail.com>
2023-10-19 02:53:10 +0530

    push
    add Readme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data";
+import Tours from './Components/Tours';
+import Refresh from './Components/Refresh';
 
-function App() {
+const App = () => {
+  const [tour, setTour] = useState(data);
+
+  function removeTour(id) {
+    const newTour = tour.filter(tour => tour.id !== id)
+    console.log(id);
+    setTour(newTour);
+  }
+
+
+  if (tour.length === 0) {
+    return <Refresh setTour={setTour} data={data}/>
+  }
+
   return (
-    <div className="text-3xl font-bold underline">
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
-  );
-}
+    <Tours tours={tour} removeTour={removeTour} />
+  )
+};
 
 export default App;
